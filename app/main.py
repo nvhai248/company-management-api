74f3f3c18ae689fea8e013b88dbc0231940e96d8
nvhai248 <nvhai2408@gmail.com>
2024-08-26 15:27:34 +0700

    fix: add cors policy

--- app/main.py
+++ app/main.py
@@ -1,10 +1,20 @@
-from typing import Union
-
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes import company, user, task
 from routes import auth
 
 app = FastAPI()
+
+# CORS configuration
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "*"
+    ],  # Allow all origins. Replace with specific origins in production.
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods. Replace with specific methods if needed.
+    allow_headers=["*"],  # Allow all headers. Replace with specific headers if needed.
+)
 
 app.include_router(company.router)
 app.include_router(user.router)
